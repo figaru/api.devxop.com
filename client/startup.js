3f3ecc09f708bd5aec2ed6168e0c00f659c96370
Daniel Abrantes <daniel.abrantes95@gmail.com>
2019-08-25 18:01:19 +0100

    commit

--- client/startup.js
+++ client/startup.js
@@ -1,2 +1,38 @@
 import Materialize from 'materialize-css';
+<<<<<<< HEAD
 global.M = global.Materialize = Materialize;
+=======
+global.M = global.Materialize = Materialize;
+
+$(function() {
+    // Handler for .ready() called.
+
+    setInterval(function(){
+        let device = Session.get("device");
+
+        if(device){
+            //ping(device);
+            //console.log(device);
+            //console.log("pinging device");
+            ping(device);
+        }else{
+            
+        }
+    }, 5000);
+});
+
+
+function ping(device){
+
+    Meteor.call("devices.ping", device);
+    /* $.ajax({
+       url: 'http://devxop.ddns.net:5000/api/ping',
+       success: function(result){
+          alert('reply');
+       },     
+       error: function(result){
+           alert('timeout/error');
+       }
+    }); */
+ }
+>>>>>>> parent of dd0a01d... interval updated ping
